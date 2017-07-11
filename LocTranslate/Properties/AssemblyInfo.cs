6cbddb72c58bd8986e3d02dc4583262ce3e588e1
MalakymR <MalakymR@esdf.uk>
2017-07-11 12:35:37 +0100

    add .gitignore

--- LocTranslate/Properties/AssemblyInfo.cs
+++ LocTranslate/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LocTranslate")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
